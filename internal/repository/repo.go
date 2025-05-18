806b2a88d4ccc667f7660c834aa2fb1a23a1ded6
Alina Pechak <a.pechak@scalablesolutions.io>
2025-05-18 17:29:38 +0400

    Fix code after comments ver.2

--- internal/repository/repo.go
+++ internal/repository/repo.go
@@ -1,6 +1,7 @@
 package repository
 
 import (
+	"errors"
 	"fmt"
 	"otus_project/internal/model"
 )
@@ -10,36 +11,36 @@ type Item interface {
 }
 
 var (
-	users       []model.User
-	projects    []model.Project
-	reminders   []model.Reminder
-	tags        []model.Tag
-	timeEntries []model.TimeEntry
-	tasks       []model.Task
+	users       []*model.User
+	projects    []*model.Project
+	reminders   []*model.Reminder
+	tags        []*model.Tag
+	timeEntries []*model.TimeEntry
+	tasks       []*model.Task
 )
 
 func SaveItem(item Item) error {
 	switch v := item.(type) {
 	case model.User:
-		users = append(users, v)
+		users = append(users, &v)
 		fmt.Println("User saved:", v)
 	case model.Project:
-		projects = append(projects, v)
+		projects = append(projects, &v)
 		fmt.Println("Project saved:", v)
 	case model.Task:
-		tasks = append(tasks, v)
+		tasks = append(tasks, &v)
 		fmt.Println("Task saved:", v)
 	case model.Reminder:
-		reminders = append(reminders, v)
+		reminders = append(reminders, &v)
 		fmt.Println("Reminder saved:", v)
 	case model.Tag:
-		tags = append(tags, v)
+		tags = append(tags, &v)
 		fmt.Println("Tag saved:", v)
 	case model.TimeEntry:
-		timeEntries = append(timeEntries, v)
+		timeEntries = append(timeEntries, &v)
 		fmt.Println("TimeEntry saved:", v)
 	default:
-		fmt.Println("No such type:", v.GetItem())
+		return errors.New(fmt.Sprintf("Error saving: %v", v.GetItem()))
 	}
 	return nil
 }
